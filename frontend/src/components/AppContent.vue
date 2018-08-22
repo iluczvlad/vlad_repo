6ded29659f30b89913dc34ca1498da123818019f
iluczvlad <iluczvlad@gmail.com>
2018-08-22 18:55:37 +0300

    fitst interface cyan barf

---
+++ frontend/src/components/AppContent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app-content">
+        <img src="@/assets/sh.jpg" class="cherry">
+        <input v-model="message">
+        <label>{{message}}</label>
+        <button @click="changeMessage">Change message</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app-content',
+  data: function () {
+    return {
+        message: '',
+    }
+  },
+  methods: {
+      changeMessage: function () {
+          this.message = 'qwerty';
+      }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+#app-content {
+    width: 100%;
+}
+.cherry {
+    width: 200px;
+
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="header">
+        <div class="logo">
+            LOGO
+        </div>
+        <div>
+            <button v-if="!isLoggedIn" @click="login">Log In</button>
+            <button v-if="!isLoggedIn">Sign Up</button>
+            <button v-if="isLoggedIn" @click="logout">Log Out</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data: function () {
+      return {
+          isLoggedIn: false,
+      }
+  },
+  methods: {
+      login: function () {
+          this.isLoggedIn = true;
+      },
+      logout: function () {
+          this.isLoggedIn = false;
+      }
+      
+  }
+};
+</script>
+
+<style lang="less" scoped>
+#header {
+    width: 100%;
+    height: 80px;
+    border-bottom: 1px solid black;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.logo {
+    width: 100px;
+    height: 80%;
+    background: green;
+}
+</style>
